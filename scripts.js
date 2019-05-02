e57e4a5ab6f5ff2c7e9c34f63458d768bd8e771e
RayBednara <44881818+RayBednara@users.noreply.github.com>
2019-05-02 16:52:23 -0500

    add code from midterm

--- scripts.js
+++ scripts.js
@@ -5,3 +5,16 @@
  * 4. Add those to an 'Array' that will hold all of the student names.
  * 5. Iterate over said 'Array', passing each of the names as arguments into console.log().
  */
+
+//code from midterm project
+
+document.querySelector("form").addEventListener("submit", ev => {
+  // capture info entered into form
+  ev.preventDefault();
+  const bsLevel = document.querySelector("#blood-sugar-level").value;
+  const comment = document.querySelector("#comments").value;
+
+  // Write data to an Object
+  const bsData = { sugarLevel: bsLevel, comments: comment };
+  console.log(bsData);
+});
