e2f0261fe0334b39d59574528ac19a9b3134e95d
Ray Bednara <ray.bednara@att.net>
2019-05-10 13:43:04 -0500

    clean up old comments

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-/**
- * 1. LISTEN for and HANDLE the 'submit' event.
- * 2. QUERY the DOCUMENT for the <form> fields and store those VALUES in variable(s).
- * 3. Be sure that both fields get concatenated into 1 'String.'
- * 4. Add those to an 'Array' that will hold all of the student names.
- * 5. Iterate over said 'Array', passing each of the names as arguments into console.log().
- */
-
 // Initializing an array (reduce() and map() functions only relate to arrays)
 const bloodSugarReadings = [];
 
@@ -33,7 +25,6 @@ document.querySelector("form").addEventListener("submit", ev => {
   let bsResults = bloodSugarInfo.map(bsdata => bsdata.bsLevel);
   Number(bsResults);
   const bloodSugarTotal = bsResults.reduce((total, curr) => (total += curr));
-  console.log(typeof bloodSugarTotal);
 
   const bloodSugarAverage = bloodSugarTotal / bsResults.length;
 });
