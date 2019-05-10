a1f8bb53f5a5f5f724661aa37a2b91a62816e7f1
Ray Bednara <ray.bednara@att.net>
2019-05-10 13:57:40 -0500

    create bs manager and log readings

--- scripts.js
+++ scripts.js
@@ -1,30 +1,42 @@
 // Initializing an array (reduce() and map() functions only relate to arrays)
-const bloodSugarReadings = [];
+// const bloodSugarReadings = [];
 
-function bloodSugarAverage(arr) {
-  const sum = arr.reduce((total, curr) => total + curr);
-  return sum / arr.length;
-}
+// function bloodSugarAverage(arr) {
+//   const sum = arr.reduce((total, curr) => total + curr);
+//   return sum / arr.length;
+// }
 
 // constructor for bloodsugar data
-function UserBloodSugar(bsLevel, comment) {
-  this.bsLevel = bsLevel;
-  this.comment = comment;
+function BloodSugarMgr() {
+  this.readings = [];
 }
+
+// comments are optional
+BloodSugarMgr.prototype.addReading = function(reading, comment) {
+  this.readings.push({
+    reading: reading,
+    comment: comment
+  });
+};
+
+const bsm = new BloodSugarMgr();
 
 document.querySelector("form").addEventListener("submit", ev => {
   // capture info entered into form
   ev.preventDefault();
+
   const bsLevel = document.querySelector("#blood-sugar-level").value;
   const comment = document.querySelector("#comments").value;
 
-  // push data into array
-  bloodSugarReadings.push(new UserBloodSugar((bsLevel, comment)));
+  bsm.addReading(bsLevel, comment);
 
-  // map the array of data collected to pull out our bslevel data
-  let bsResults = bloodSugarInfo.map(bsdata => bsdata.bsLevel);
-  Number(bsResults);
-  const bloodSugarTotal = bsResults.reduce((total, curr) => (total += curr));
+  // // push data into array
+  // bloodSugarReadings.push(new UserBloodSugar((bsLevel, comment)));
 
-  const bloodSugarAverage = bloodSugarTotal / bsResults.length;
+  // // map the array of data collected to pull out our bslevel data
+  // let bsResults = bloodSugarInfo.map(bsdata => bsdata.bsLevel);
+  // Number(bsResults);
+  // const bloodSugarTotal = bsResults.reduce((total, curr) => (total += curr));
+
+  // const bloodSugarAverage = bloodSugarTotal / bsResults.length;
 });
