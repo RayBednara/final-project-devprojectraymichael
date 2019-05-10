4530b15dca846bb29362c415f8f6319a2846fd1e
Ray Bednara <ray.bednara@att.net>
2019-05-10 13:39:44 -0500

    clean up code comments

--- scripts.js
+++ scripts.js
@@ -6,18 +6,14 @@
  * 5. Iterate over said 'Array', passing each of the names as arguments into console.log().
  */
 
-// code from midterm project
-
 // Initializing an array (reduce() and map() functions only relate to arrays)
-const bloodSugarInfo = [];
-
-// Initializing an array purely to retrieve averages of all applicants.
-const bsAvgArr = [];
+const bloodSugarReadings = [];
 
 function bloodSugarAverage(arr) {
   const sum = arr.reduce((total, curr) => total + curr);
   return sum / arr.length;
 }
+
 // constructor for bloodsugar data
 function UserBloodSugar(bsLevel, comment) {
   this.bsLevel = bsLevel;
@@ -29,23 +25,15 @@ document.querySelector("form").addEventListener("submit", ev => {
   ev.preventDefault();
   const bsLevel = document.querySelector("#blood-sugar-level").value;
   const comment = document.querySelector("#comments").value;
+
   // push data into array
-  bloodSugarInfo.push(new UserBloodSugar((bsLevel, comment)));
-
-  //console.log(bloodSugarInfo);
-
-  // the accumulator is concatinating strings instead of treating the data as a number
+  bloodSugarReadings.push(new UserBloodSugar((bsLevel, comment)));
 
   // map the array of data collected to pull out our bslevel data
   let bsResults = bloodSugarInfo.map(bsdata => bsdata.bsLevel);
   Number(bsResults);
-  // reduce the total bs data into one collection of numbers
   const bloodSugarTotal = bsResults.reduce((total, curr) => (total += curr));
-  console.log(typeof(bloodSugarTotal))
-  // average our collection into a usable number
+  console.log(typeof bloodSugarTotal);
 
   const bloodSugarAverage = bloodSugarTotal / bsResults.length;
-  // console.log(bloodSugarTotal);
-  // console.log(bsResults.length);
-  // console.log(bloodSugarAverage);
 });
